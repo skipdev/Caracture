9e41883ee46300d270947a3a82362340b7a9b700
Skip <skip.barden@gmail.com>
2020-10-19 15:12:18 +0100

    setup

---
+++ src/stories/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './header.css';
+import {Navbar} from "./Navbar";
+import {Logo} from "./Logo";
+
+export const Header = () => (
+    <header>
+      <div className={'header__logo--wrapper'}>
+        <Logo/>
+      </div>
+      <div className={'header__navbar--wrapper'}>
+        <Navbar/>
+      </div>
+    </header>
+);
