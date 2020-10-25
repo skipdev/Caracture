5931f9b83d8b904675938403e750e26688483faf
Skip <skip.barden@gmail.com>
2020-10-25 19:38:56 +0000

    tweaks

--- src/stories/Carousel.js
+++ src/stories/Carousel.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import slide1 from '../slide1.png';
+import slide1 from '../slide1.jpg';
 import slide2 from '../slide2.jpg';
 import slide3 from '../slide3.jpg';
+import slide4 from '../slide4.jpg';
 import {Button} from "./Button";
 
 export const ControlledCarousel = () => {
@@ -22,7 +23,7 @@ export const ControlledCarousel = () => {
               alt="First slide"
           />
           <Carousel.Caption>
-            <h3>High-quality personalised accessories, specially made for Range Rover</h3>
+            <h3>Call us now to find out about high-quality personalisation for your Range Rover</h3>
             <Button type={"secondary"} label={'Get in touch'}/>
           </Carousel.Caption>
         </Carousel.Item>
@@ -34,8 +35,8 @@ export const ControlledCarousel = () => {
           />
 
           <Carousel.Caption>
-            <h3>Professionally fitted carbon fibre interiors</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <h3>Professionally fitted carbon fibre</h3>
+            <p>Applicable to both the interior and exterior</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
@@ -47,9 +48,17 @@ export const ControlledCarousel = () => {
 
           <Carousel.Caption>
             <h3>Sweep indicator upgrades</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+              className="d-block w-100"
+              src={slide4}
+              alt="Fourth slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Tread plate personalisation</h3>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
